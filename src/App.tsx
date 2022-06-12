ef1f0b564c3a7587e3862a16830057c245d82aa9
Prabu <prabu.mgks@gmail.com>
2022-06-12 13:04:56 +0530

    mob nav design done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import logo from "./images/logo.svg";
+
+import iconMenu from "./images/icon-menu.svg";
+import iconCart from "./images/icon-cart.svg";
+
+import imageAvatar from "./images/image-avatar.png";
+
 function App() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <>
+      <header className="px-6 py-5 flex items-center justify-between">
+        <div className="flex gap-4 items-center">
+          <button className="" aria-label="menu button">
+            <img src={iconMenu} alt="" />
+          </button>
+          <div className="w-[8.625rem]">
+            <img src={logo} alt="" />
+          </div>
+        </div>
+        <div className="flex items-center gap-5">
+          <button className="" aria-label="cart button">
+            <img src={iconCart} alt="" />
+          </button>
+          <div className="rounded-full w-6">
+            <img src={imageAvatar} alt="" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default App;
