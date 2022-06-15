984df5e6e198681357c6942b75cf887bdf0fed4d
Prabu <prabu.mgks@gmail.com>
2022-06-15 17:34:28 +0530

    created product model and data

---
+++ src/data/products.ts
@@ -0,0 +1,30 @@
+import { Product } from "../models/product";
+
+import imageProduct1 from "../images/image-product-1.jpg";
+import imageProduct2 from "../images/image-product-2.jpg";
+import imageProduct3 from "../images/image-product-3.jpg";
+import imageProduct4 from "../images/image-product-4.jpg";
+
+import imageProductThumb1 from "../images/image-product-thumbnail-1.jpg";
+import imageProductThumb2 from "../images/image-product-thumbnail-2.jpg";
+import imageProductThumb3 from "../images/image-product-thumbnail-3.jpg";
+import imageProductThumb4 from "../images/image-product-thumbnail-4.jpg";
+
+export const Products: Product[] = [
+  {
+    brand: "Sneaker Company",
+    title: "Fall Limited Edition Sneakers",
+    desc: `These low-profile sneakers are your perfect casual wear companion.
+    Featuring a durable rubber outer sole, they'll withstand everything
+    the weather can offer.`,
+    price: 250,
+    discount: 50,
+    images: [imageProduct1, imageProduct2, imageProduct3, imageProduct4],
+    thumbnails: [
+      imageProductThumb1,
+      imageProductThumb2,
+      imageProductThumb3,
+      imageProductThumb4,
+    ],
+  },
+];
---
+++ src/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  brand: string;
+  title: string;
+  desc: string;
+  price: number;
+  discount: number;
+  images: string[];
+  thumbnails: string[];
+}
